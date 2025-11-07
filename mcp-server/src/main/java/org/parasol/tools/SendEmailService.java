9e73d7b2a32286f8380304276fe1655a6beb17a0
Eric Deandrea <eric.deandrea@gmail.com>
2025-11-07 10:10:08 -0500

    Remove validation stuff

--- mcp-server/src/main/java/org/parasol/tools/SendEmailService.java
+++ mcp-server/src/main/java/org/parasol/tools/SendEmailService.java
@@ -1,9 +1,6 @@
 package org.parasol.tools;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
@@ -12,19 +9,18 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
-import io.quarkiverse.mcp.server.Tool;
-import io.quarkiverse.mcp.server.ToolArg;
 import io.quarkiverse.mcp.server.ToolResponse;
 
 import dev.langchain4j.agent.tool.P;
+import dev.langchain4j.agent.tool.Tool;
 
 @ApplicationScoped
 public class SendEmailService {
   public record Email(
-      @P("The to address") @NotEmpty String to,
-      @P("The from address") @NotEmpty String from,
-      @P("The email subject") @NotEmpty String subject,
-      @P("The email body") @NotEmpty String body) {}
+      @P("The to address") String to,
+      @P("The from address") String from,
+      @P("The email subject") String subject,
+      @P("The email body") String body) {}
 
   private final Mailer mailer;
   private final Tracer tracer;
@@ -34,8 +30,10 @@ public class SendEmailService {
     this.tracer = tracer;
   }
 
-  @Tool(name = "sendEmail", description = "Sends an email")
-  public ToolResponse sendEmail(@ToolArg(name = "email", description = "The email") @NotNull @Valid Email email) {
+  @Tool(name = "sendEmail", value = "Sends an email")
+//  @Tool(name = "sendEmail", description = "Sends an email")
+  public ToolResponse sendEmail(Email email) {
+//  public ToolResponse sendEmail(@ToolArg(name = "email", description = "The email") Email email) {
     var span = this.tracer.spanBuilder("sendEmail")
         .setAttribute("arg.email", email.toString())
         .setParent(Context.current().with(Span.current()))
